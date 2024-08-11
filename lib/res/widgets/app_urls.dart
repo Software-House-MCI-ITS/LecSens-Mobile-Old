c0926dfb773354d7bdb23713286f751578abc7de
borednuna <hanunshaka02@gmail.com>
2024-08-12 01:08:11 +0700

    feat: temporary endpoints and base network function

---
+++ lib/res/widgets/app_urls.dart
@@ -0,0 +1,6 @@
+class AppUrls {
+  static const baseUrl = "https://localhost:8080";
+  static const loginEndPoint = "$baseUrl/users/login";
+  static const registerEndPoint = "$baseUrl/users/register";
+  static const emailVerificationEndpoint = "$baseUrl/users/verification-email";
+}
